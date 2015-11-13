5ce2d589c05dcdfb2012abd395a5fce0e2c6a87b
noel-arce <n.arcellana.00@gmail.com>
2015-11-12 17:12:23 -0800

    created city show page

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -3,4 +3,28 @@ class CitiesController < ApplicationController
 		@cities = City.all
 	end
 
+	def show
+		@city = City.find(params[:id])
+		@posts = Post.where(city_id:(@city))
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(post_params)
+
+		if @post.save
+			redirect_to city_path(@city)
+		else
+			redirect_to new_post_path
+	end
+
+	private
+
+		def post_params
+			params.require(:post).permit(:title, :content)
+		end
+
 end
