5e6269f8a413c406df59f5451a1f204fc488176a
jorgeRafaelSJ <jorgerafaelsj@gmail.com>
2015-11-12 20:23:55 -0800

    create new post form, general links

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -8,24 +8,4 @@ class CitiesController < ApplicationController
 		@posts = Post.where(city_id:(@city))
 	end
 
-	def new
-		@post = Post.new
-	end
-
-	def create
-		@post = Post.new(post_params)
-
-		if @post.save
-			redirect_to city_path(@city)
-		else
-			redirect_to new_post_path
-	end
-
-	private
-
-		def post_params
-			params.require(:post).permit(:title, :content)
-		end
-	end
-
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,23 @@
 class PostsController < ApplicationController
+	def new
+		@post = Post.new	
+		render :new
+	end
+
+	def create
+		@post = Post.new(post_params)
+#need to define @city
+		if @post.save
+			# redirect_to city_path(@city)
+		else
+			# redirect_to new_post_path
+	end
+
+	private
+
+		def post_params
+			params.require(:post).permit(:title, :content)
+		end
+	end
 end
+
