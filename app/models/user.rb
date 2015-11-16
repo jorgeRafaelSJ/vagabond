88669b3b40649dfdfb3138b0590f5a31a09f7847
John Costa <johncosta@Johns-MacBook-Pro.local>
2015-11-16 10:34:23 -0800

    default image resizing working

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
 	has_many :posts, dependent: :destroy
 	has_secure_password
 	validates :email, uniqueness: true
-	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "http://i.imgur.com/cepxUEY.jpg"
+	has_attached_file :avatar, styles: { medium: "300x300>" }, default_url: "http://i.imgur.com/cepxUEY.jpg"
+	has_attached_file :avatarthumb, styles: { thumb: "100x100>" }, default_url: "http://i.imgur.com/wBmYDiQ.jpg"
 	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 	validates_attachment_file_name :avatar, matches: [/png\Z/, /jpe?g\Z/]
 	do_not_validate_attachment_file_type :avatar
